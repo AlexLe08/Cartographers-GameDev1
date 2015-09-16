1344ff57bc9717f9fb0e42c79a95a376831c4141
Will Brennan <galacticonrex@yahoo.com>
2015-09-16 16:27:08 -0400

    Hello friends here is the game

--- Code/src/app.js
+++ Code/src/app.js
@@ -2,8 +2,12 @@ var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var layer = new uiLayer();
-        this.addChild(layer);
+        this.addChild(layer, 100);
 
+		var col = cc.LayerColor.create( new cc.Color( 90,100,255,255 )  );
+        var layer = new GameLayer();
+		this.addChild(col, 4);
+        this.addChild(layer, 5);
     }
 });
 
--- Code/src/resource.js
+++ Code/src/resource.js
@@ -1,6 +1,7 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
 	Close_png: "res/close.png",
+	boat_png : "res/sprite_boat.png",
 };
 
 var g_resources = [];
